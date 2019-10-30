b00d606af9241a6724643e592974aa2bc8d0cb49
KubatAbyshov <flimdeweack@gmail.com>
2019-10-30 16:45:01 +0600

    горизонтальный калькулятор

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         resultField = (TextView) findViewById(R.id.resultField);
         numberField = (EditText) findViewById(R.id.numberField);
-        clear=findViewById(R.id.clean);
+        clear = findViewById(R.id.clean);
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,80 +33,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState){
+    protected void onSaveInstanceState(Bundle outState) {
         outState.putString("OPERATION", lastOperation);
 
         if (operand != null)
-            outState.putDouble("OPERAND",operand);
+            outState.putDouble("OPERAND", operand);
         super.onSaveInstanceState(outState);
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState){
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         lastOperation = savedInstanceState.getString("OPERATION");
         operand = savedInstanceState.getDouble("OPERAND");
         resultField.setText(operand.toString());
     }
 
-    public void onNumberClick (View view){
-        Button button = (Button)view;
+    public void onNumberClick(View view) {
+        Button button = (Button) view;
         numberField.append(button.getText());
 
-        if (lastOperation.equals("=")&& operand!=null){
+        if (lastOperation.equals("=") && operand != null) {
             operand = null;
         }
     }
-    public void onOperationClick(View view){
-        Button button = (Button)view;
+
+    public void onOperationClick(View view) {
+        Button button = (Button) view;
         String op = button.getText().toString();
         String number = numberField.getText().toString();
 
-        if (number.length()>0){
-            number = number.replace(',','.');
+        if (number.length() > 0) {
+            number = number.replace(',', '.');
             try {
-                performOperation(Double.valueOf(number),op);
-            }catch (NumberFormatException ex){
+                performOperation(Double.valueOf(number), op);
+            } catch (NumberFormatException ex) {
                 numberField.setText("");
             }
         }
         lastOperation = op;
     }
 
-    private void performOperation(Double number, String operation){
-        if (operand == null){
+    private void performOperation(Double number, String operation) {
+        if (operand == null) {
             operand = number;
-        }
-        else{
-            if (lastOperation.equals("=")){
+        } else {
+            if (lastOperation.equals("=")) {
                 lastOperation = operation;
 
             }
 
-            switch (lastOperation){
+            switch (lastOperation) {
                 case "=":
                     operand = number;
                     break;
                 case "÷":
-                    if (number == 0){
+                    if (number == 0) {
                         operand = 0.0;
+                    } else {
+                        operand /= number;
                     }
-                    else {
-                        operand /=number;
-                    }
+                    break;
+
+                case "%":
+                    number /=100;
+                    operand *= number;
                     break;
                 case "×":
                     operand *= number;
                     break;
                 case "+":
-                    operand +=number;
+                    operand += number;
                     break;
                 case "-":
-                    operand -=number;
+                    operand -= number;
                     break;
+
             }
         }
-        resultField.setText(operand.toString().replace('.',','));
+        resultField.setText(operand.toString().replace('.', ','));
         numberField.setText("");
     }
 }
